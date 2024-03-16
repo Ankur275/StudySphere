d7ed0817ba937fb88024d20394f87be27dd9d05d
Ankur <ankurchakrajit@gmail.com>
2024-03-17 02:14:13 +0530

    route import edit

--- src/routes/signUp.route.js
+++ src/routes/signUp.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-// import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 import { registerUser } from "../controllers/signUp.Controllers.js";
 
 const router = Router()
