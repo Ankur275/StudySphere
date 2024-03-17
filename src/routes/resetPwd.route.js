2eb0cba14618eab5790b355178dbe5a11af73778
Ankur <ankurchakrajit@gmail.com>
2024-03-17 15:31:23 +0530

    path change

--- src/routes/resetPwd.route.js
+++ src/routes/resetPwd.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 import { resetPassword } from "../controllers/resetPass.Controller.js";
 
 const router = Router()
